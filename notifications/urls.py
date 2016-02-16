b567a2ffe858f57141e9580eefc13311dbc96d9c
Billy Reynolds <breynolds@amplify-nation.com>
2016-02-16 14:08:40 -0500

    django 19 upgrades

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import *
+from django.conf import urls
+from .views import all, unread, mark_all_as_read, mark_as_read, mark_as_unread
 
-urlpatterns = patterns('notifications.views',
-    url(r'^$', 'all', name='all'),
-    url(r'^unread/$', 'unread', name='unread'),
-    url(r'^mark-all-as-read/$', 'mark_all_as_read', name='mark_all_as_read'),
-    url(r'^mark-as-read/(?P<slug>\d+)/$', 'mark_as_read', name='mark_as_read'),
-    url(r'^mark-as-unread/(?P<slug>\d+)/$', 'mark_as_unread', name='mark_as_unread'),
+urlpatterns = urls.patterns(
+    '',
+    urls.url(
+        r'^',
+        all,
+        name='all'
+    ),
+    urls.url(
+        r'^unread/$',
+        unread,
+        name='unread'
+    ),
+    urls.url(
+        r'^mark-all-as-read/$',
+        mark_all_as_read,
+        name='mark_all_as_read'
+    ),
+    urls.url(
+        r'^mark-as-read/(?P<slug>\d+)/$',
+        mark_as_read,
+        name='mark_as_read'
+    ),
+    urls.url(
+        r'^mark-as-unread/(?P<slug>\d+)/$',
+        mark_as_unread,
+        name='mark_as_unread'
+    ),
 )
--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from distutils.core import setup
-from notifications import __version__
 
-setup(name='django-notifications-hq',
-      version=__version__,
-      description='GitHub notifications alike app for Django.',
-      long_description=open('README.rst').read(),
-      author='Brant Young',
-      author_email='brant.young@gmail.com',
-      url='http://github.com/brantyoung/django-notifications',
-      install_requires=[
-          'django>=1.4',
-          'django-model-utils>=2.0.3'
-      ],
-      packages=['notifications',
-                'notifications.templatetags',
-                'notifications.migrations'
-               ],
-      package_data={'notifications': [
-                                 'templates/notifications/*.html']},
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='django-notifications-hq',
+    version='1.0.0rc1',
+    description='GitHub notifications alike app for Django.',
+    long_description=open('README.rst').read(),
+    author='Amplify Nation',
+    author_email='tech@amplify-nation.com',
+    url='http://github.com/amplify-nation/django-notifications',
+    install_requires=[
+      'django>=1.9.2',
+      'django-model-utils==2.4'
+    ],
+    packages=['notifications',
+            'notifications.templatetags',
+            'notifications.migrations'
+           ],
+    package_data={'notifications': [
+                             'templates/notifications/*.html']},
+    classifiers=['Development Status :: 5 - Production/Stable',
+               'Environment :: Web Environment',
+               'Framework :: Django',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: BSD License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Topic :: Utilities'],
+)
